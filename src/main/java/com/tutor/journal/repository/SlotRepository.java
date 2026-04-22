8ba132b3a0c6e208982de3e98f899dcdadf9a9aa
duenfx <karinahanzha@gmail.com>
2026-04-22 17:33:59 +0300

    add tutor search method to SlotRepository

--- src/main/java/com/tutor/journal/repository/SlotRepository.java
+++ src/main/java/com/tutor/journal/repository/SlotRepository.java
@@ -2,6 +2,8 @@ package com.tutor.journal.repository;
 
 import com.tutor.journal.entity.Slot;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
 public interface SlotRepository extends JpaRepository<Slot, Long> {
+    List<Slot> findByTutorId(Long tutorId);
 }
