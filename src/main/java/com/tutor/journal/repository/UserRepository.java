77a8930cba929d3b1befa36b587a5126aaaacd40
duenfx <karinahanzha@gmail.com>
2026-05-10 10:46:02 +0300

    feat: add User entity and repository

---
+++ src/main/java/com/tutor/journal/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.tutor.journal.repository;
+
+import com.tutor.journal.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
