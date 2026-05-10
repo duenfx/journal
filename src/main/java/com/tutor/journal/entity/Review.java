ee3bee692d5520d0bede7c7ffce9d419d01cf82d
duenfx <karinahanzha@gmail.com>
2026-05-10 11:47:05 +0300

    feat: add review system

---
+++ src/main/java/com/tutor/journal/entity/Review.java
@@ -0,0 +1,57 @@
+package com.tutor.journal.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Review {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long tutorId;
+    private String studentName;
+    private Integer rating; // 1-5
+    private String comment;
+
+    public Review() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getTutorId() {
+        return tutorId;
+    }
+
+    public void setTutorId(Long tutorId) {
+        this.tutorId = tutorId;
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+
+    public Integer getRating() {
+        return rating;
+    }
+
+    public void setRating(Integer rating) {
+        this.rating = rating;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+}
